c037d190dfd2e72cc712ec2765bb02e458ecd77e
tushardigitalcollab <109473842+tushardigitalcollab@users.noreply.github.com>
2022-08-02 21:11:08 +1200

    Release v1.0 (#4)
    * Renaming the homepage
    
    * Minimal Identity feature
    
    using Microsoft Identity Control + DB connectivity
    
    * Constructs Dashboard Navigation
    
    * Basic App Scaffolding and Additional Features
    
    * Updating SDK to .Net 5
    
    * Fixing SDK Version
    
    * Update dotnet.yml
    
    * Ameding csproj file
    
    Co-authored-by: devteamdigitalcollab <109469955+devteamdigitalcollab@users.noreply.github.com>

---
+++ Models/PropertyInfo.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver.Core.Events;
+
+namespace PropertyInspection_WebApp.Models
+{
+    public class PropertyInfo
+    {
+
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string PropertyId { get; set; } = ObjectId.GenerateNewId().ToString();
+
+        [BsonElement("PropertyAddress")]
+        public string PropertyAddress { get; set; }
+
+        [BsonElement("ClientFName")]
+        public string ClientFName { get; set; }
+
+        [BsonElement("ClientLName")]
+        public string ClientLName { get; set; }
+
+        [BsonElement("InspectionType")]
+        public string InspectionType { get; set; }
+
+        [BsonElement("InspectionDate")]
+        [BindProperty, DataType(DataType.Date)]
+        public DateTime InspectionDate { get; set; }
+
+        [BsonElement("InspectionTime")]
+        [BindProperty, DataType(DataType.Time)]
+        public DateTime InspectionTime { get; set; }
+
+        [BsonElement("WhenAdded")]
+        public DateTime WhenAdded { get; set; }
+
+        [BsonElement("AddedBy")]
+        public string AddedBy { get; set; }
+    }
+}
+
---
+++ Repository/PropertyInfoRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using AspNetCore.Identity.MongoDbCore.Infrastructure;
+using MongoDB.Driver;
+using PropertyInspection_WebApp.IRepository;
+using PropertyInspection_WebApp.Models;
+using PropertyInspection_WebApp.Settings;
+
+namespace PropertyInspection_WebApp.Repository
+{
+    public class PropertyInfoRepository : IPropertyInfoRepository
+    {
+        // Private Members
+        private MongoClient _mongoClient = null;
+        private IMongoDatabase _mongoDatabase = null;
+        private IMongoCollection<PropertyInfo> _propertyInfoTable = null;
+
+        //DBConfig Injected
+        public PropertyInfoRepository(MongoDBConfig mongoDBConfig)
+        {
+            _mongoClient = new MongoClient(mongoDBConfig.ConnectionString);
+            _mongoDatabase = _mongoClient.GetDatabase(mongoDBConfig.PropertyDatabaseName);
+            _propertyInfoTable = _mongoDatabase.GetCollection<PropertyInfo>(mongoDBConfig.PropertyInfoTableName);
+
+        }
+
+        public string Delete(string PropertyId)
+        {
+            _propertyInfoTable.DeleteOne(x => x.PropertyId == PropertyId);
+            return "Deleted";
+        }
+
+        public PropertyInfo Get(string PropertyId)
+        {
+            return _propertyInfoTable.Find(x => x.PropertyId == PropertyId).FirstOrDefault();
+        }
+
+        public List<PropertyInfo> Gets()
+        {
+            return _propertyInfoTable.Find(FilterDefinition<PropertyInfo>.Empty).ToList();
+        }
+
+        public PropertyInfo Save(PropertyInfo propertyinfo)
+        {
+            var propertyInfoObj = _propertyInfoTable.Find(x => x.PropertyId == propertyinfo.PropertyId).FirstOrDefault();
+            if (propertyInfoObj == null)
+                _propertyInfoTable.InsertOne(propertyinfo);
+            else
+                _propertyInfoTable.ReplaceOne(x => x.PropertyId == propertyinfo.PropertyId, propertyinfo);
+            return propertyinfo;
+        }
+    }
+}
+
