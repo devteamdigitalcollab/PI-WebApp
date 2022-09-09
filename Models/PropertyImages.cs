bf67174b8daa9a241c9b32e37b043d9dc397f96f
tushardigitalcollab <109473842+tushardigitalcollab@users.noreply.github.com>
2022-09-10 00:11:49 +1200

    ModularForm and Backend Processing
    -Responsive Container
    -Creates Helpers and enabled viewbag on db insert
    -Enables ViewBag for Modular, adds upcoming section
    -ModularForm and Backend Processing
    -Identity traversal between controller
    -Reference system task.threading namespace
    -Inspection and property properties split
    -PropertyImages model creation
    -RepositoryID creation for property
    -DB config additions
    -Property Image File
    -PropertyInspection View creation
    -Nuget package updates

---
+++ Models/PropertyImages.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace PropertyInspection_WebApp.Models
+{
+    public class PropertyImages
+    {
+
+        /// <summary>
+        /// Foreign Key 
+        /// </summary>
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string PropertyId { get; set; }
+
+        [Required]
+        [BsonElement("ImageName")]
+        public string ImageName { get; set; }
+
+        [Required]
+        [BsonElement("Image")]
+        public string ImageContent { get; set; }
+
+    }
+}
+
