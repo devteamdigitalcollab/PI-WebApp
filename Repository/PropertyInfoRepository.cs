2fde3d6c3c58ee9067040df4b31427f62219ea50
DC\Tushar Pai <tushar@digitalcollab.co.nz>
2022-10-09 03:52:44 +1300

    ImageGridFS helper Class
    Creates a generic helper class for image insertions - Code reusability

--- Repository/PropertyInfoRepository.cs
+++ Repository/PropertyInfoRepository.cs
@@ -24,73 +24,93 @@ namespace PropertyInspection_WebApp.Repository
 {
     public class PropertyInfoRepository : IPropertyInfoRepository
     {
-        // Private Members
+        #region Private Members
         private MongoClient _mongoClient = null;
         private IMongoDatabase _mongoDatabase = null;
         private IMongoCollection<PropertyInfo> _propertyInfoTable = null;
-        private GridFSBucket _gridFSBucket;
-        private PropertyInfo _propertyInfo;
-        private string _bucketName = null;
-        private int _chunckSize = 0;
-
-
+        #endregion  Private Members
 
         //DBConfig Injected
         public PropertyInfoRepository(PIConfigurations pIConfigurations)
         {
-            _mongoClient = new MongoClient(pIConfigurations.ConnectionString);
-            _mongoDatabase = _mongoClient.GetDatabase(pIConfigurations.PropertyDatabaseName);
-            _propertyInfoTable = _mongoDatabase.GetCollection<PropertyInfo>(pIConfigurations.PropertyInfoTableName);
-            _bucketName = pIConfigurations.PROPERTYINFOBUCKETNAME;
-            _chunckSize = pIConfigurations.CHUNCKSIZE;
+            _mongoClient = new MongoClient(pIConfigurations.CONNECTIONSTRING);
+            _mongoDatabase = _mongoClient.GetDatabase(pIConfigurations.PROPERTYDATABASENAME);
+            _propertyInfoTable = _mongoDatabase.GetCollection<PropertyInfo>(pIConfigurations.PROPERTYINFOTABLE);
+
         }
 
+        /// <summary>
+        /// Deletes a single property using the PropertyId
+        /// </summary>
+        /// <param name="PropertyId"></param>
+        /// <returns></returns>
+        /// <exception cref="DeleteException"></exception>
         public string Delete(string PropertyId)
         {
-            _propertyInfoTable.DeleteOne(x => x.PropertyId == PropertyId);
-            return "Deleted";
+            try
+            {
+                _propertyInfoTable.DeleteOne(x => x.PropertyId == PropertyId);
+                return "Deleted";
+            }
+            catch (Exception ex)
+            {
+                throw new DeleteException(ex.Message);
+            }
         }
 
+        /// <summary>
+        /// Gets the property information for a single property using the PropertyId
+        /// </summary>
+        /// <param name="PropertyId"></param>
+        /// <returns></returns>
+        /// <exception cref="GetsException"></exception>
         public PropertyInfo Get(string PropertyId)
         {
-            return _propertyInfoTable.Find(x => x.PropertyId == PropertyId).FirstOrDefault();
+            try
+            {
+                return _propertyInfoTable.Find(x => x.PropertyId == PropertyId).FirstOrDefault();
+            }
+            catch (Exception ex)
+            {
+                throw new GetsException(ex.Message);
+            }
         }
 
+        /// <summary>
+        /// Gets all the properties listed in the databse
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="GetsException"></exception>
         public List<PropertyInfo> Gets()
         {
-            return _propertyInfoTable.Find(FilterDefinition<PropertyInfo>.Empty).ToList();
+            try
+            {
+                return _propertyInfoTable.Find(FilterDefinition<PropertyInfo>.Empty).ToList();
+            }
+            catch (Exception ex)
+            {
+                throw new GetsException(ex.Message);
+            }
         }
 
+        /// <summary>
+        /// Saves a single property to the databse using the propertyinfo object
+        /// </summary>
+        /// <param name="propertyinfo"></param>
+        /// <returns></returns>
+        /// <exception cref="SaveException"></exception>
         public bool Save(PropertyInfo propertyinfo)
         {
             try
             {
                 propertyinfo.PropertyId = ObjectId.GenerateNewId().ToString();
-                var filetToUpload = ImageProcessingHelper.ProcessImageForDBInsert(propertyinfo.RawImageFile);
-                //Instansiates GridFSBucket
                 var filename = propertyinfo.RawImageFile.FileName;
 
-                _gridFSBucket = new GridFSBucket(_mongoDatabase, new GridFSBucketOptions
-                {
-                    BucketName = _bucketName,
-                    ChunkSizeBytes = _chunckSize,
-                    WriteConcern = WriteConcern.WMajority,
-                    ReadPreference = ReadPreference.Secondary
-                });
-                var options = new GridFSUploadOptions
-                {
-                    ChunkSizeBytes = 64512, // 63KB
-                    Metadata = new BsonDocument
-                    {
-                        { "resolution", "1080P" },
-                        { "copyrighted", true }
-                    }
-                };
-                //Uploads to DB using GridFS and return string ID
-                var id = _gridFSBucket.UploadFromBytes(filename, filetToUpload, options);
+                //Uses Image processing helpers to prepare image for DB insert
+                var fileToUpload = ImageProcessingHelper.ProcessImageForDBInsert(propertyinfo.RawImageFile);
 
-                //Sets ID to proprerty model
-                propertyinfo.ImageGridFSID = Convert.ToString(id);
+                //Inserts Image to GridFS DB
+                propertyinfo.ImageGridFSID = ImageGridFSHelper.InsertImageToGridFS(_mongoDatabase, filename, fileToUpload); ;
 
                 // Check if value already exists in DB, IF not create new ELSE relpace existing
                 var result = _propertyInfoTable.Find(x => x.PropertyId == propertyinfo.PropertyId).FirstOrDefault();
@@ -106,11 +126,59 @@ namespace PropertyInspection_WebApp.Repository
             }
             catch (Exception ex)
             {
-                throw new BsonException("Error:", ex);
+                throw new SaveException(ex.Message);
             }
         }
 
 
     }
-}
 
+    #region Custom Exceptions
+
+    /// <summary>
+    /// An excpetion for Save propety to DB failures
+    /// </summary>
+    public class SaveException : Exception
+    {
+        public SaveException(string message)
+           : base(message)
+        {
+        }
+    }
+
+    /// <summary>
+    /// An excpetion for Get property from DB failures
+    /// </summary>
+    public class GetException : Exception
+    {
+        public GetException(string message)
+           : base(message)
+        {
+        }
+    }
+
+    /// <summary>
+    /// An excpetion for Get all properties from DB failures
+    /// </summary>
+    public class GetsException : Exception
+    {
+        public GetsException(string message)
+           : base(message)
+        {
+        }
+    }
+
+
+    /// <summary>
+    /// An excpetion for Delete property  failures
+    /// </summary>
+    public class DeleteException : Exception
+    {
+        public DeleteException(string message)
+           : base(message)
+        {
+        }
+    }
+    #endregion Custom Exceptions
+
+}
